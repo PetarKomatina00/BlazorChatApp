0856d0a80547c5d0d0d9f5ce6c221cc928ddf07a
Petar Koma <tobygames855@gmail.com>
2024-05-02 16:18:32 +0200

    Initial commit

---
+++ BlazorWebAppChatApp/BlazorWebAppChatApp/ChatHub/ChatApp.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Runtime.CompilerServices;
+
+namespace BlazorWebAppChatApp.ChatHub
+{
+    public class ChatApp : Hub
+    {
+        public async Task Broadcast(string username, string message)
+        {
+            await Clients.All.SendAsync("Broadcast", username, message);
+        }
+    }
+}
